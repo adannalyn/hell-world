8f825be6f6c116423d50b0c501b08a48db7c9c2e
lyndatcd <lyndatcd@users.noreply.github.com>
2022-12-23 04:31:52 +0100

    learning in bootcamp

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
+const bodyParser = require('body-parser');
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors')
 const app = express();
 require('dotenv/config');
 
+app.use(cors());
+app.use(bodyParser.json());
 
-app.get('/', (req,res) => {
-  res.send('We are on home');
-});
+const postRoute = require('./routes/posts');
+app.use('/posts', postRoute);
+
 app.get('/', (req, res) => {
-  res.send('We are on posts');
+  res.send('We are on home');
 });
 
 mongoose.connect(process.env.DB_CONNECT, mongoose.set('strictQuery', true),() =>{

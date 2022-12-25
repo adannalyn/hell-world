ca3ef00c3b1f3ccf7be89054bcf1c0bf68ba8179
lyndatcd <lyndatcd@users.noreply.github.com>
2022-12-25 18:22:03 +0100

    made some api adjustments

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require ('express');
 const app = express();
 app.use(express.json());
 
+app.use(express.static('public'))
+
 const jwt = require('jsonwebtoken');
 
 const cors = require ('cors');

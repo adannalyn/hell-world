b66d111c540c8602e7018a5d383d2c08530feda0
lyndatcd <qvexvon@gmail.com>
2022-12-25 11:40:54 +0100

    commit

---
+++ config/database.js
@@ -0,0 +1,12 @@
+const mongoose = require ('mongoose');
+
+const dbConnect = async () => {
+	try {
+		mongoose.connect(process.env.MONGO_URI, mongoose.set('strictQuery', true),() => {
+			console.log('connected to MongoDB! ');
+		});
+	} catch (error) {
+		console.log(error.message);        }                                  };
+
+
+module.exports = dbConnect;

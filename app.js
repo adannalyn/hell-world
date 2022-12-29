9550e442fd6b1ad558b17e00077fa733c71355bc
lyndatcd <lyndatcd@users.noreply.github.com>
2022-12-29 19:07:11 +0100

    merge

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 app.use(express.static('public'));
 app.use(express.json({ limit: "50mb" }));
 
+
 const cors = require ('cors');
 app.use(cors());
 
--- routes/job_filter.js
+++ routes/job_filter.js
@@ -3,7 +3,9 @@ const express = require("express");
 const router = express.Router();       
 
 const options = {
-  method: 'GET',
+  'Access-Control-Allow-Method': 'GET',
+  'Access-Control-Allow-Origin': '*',
+  'Content-Type': 'application/json',
   url: 'https://jsearch.p.rapidapi.com/search-filters',
   params: {query: 'Python developer in Texas, USA'},
   headers: {
@@ -13,9 +15,9 @@ const options = {
 };
 
 axios.request(options).then(function (response) {
-	//console.log(response.data);
+	// console.log(response.data);
 }).catch(function (error) {
-	//console.error(error);
+	// console.error(error);
 });
 
 router.get('/', (req, res) =>{

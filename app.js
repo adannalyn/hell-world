085efc23aaa142b54af34dd570a43224bb58b9c6
lyndatcd <qvexvon@gmail.com>
2023-01-04 09:28:45 +0100

    user role

--- app.js
+++ app.js
@@ -10,18 +10,18 @@ app.use(cors());
 const bodyParser = require('body-parser')
 app.use(bodyParser.json());
 
-// const auth = require("./middleware/auth");
-// app.use(auth);
+const auth = require("./middleware/auth");
+app.use(auth);
 
-// const corsOptions = {
-//   origin: 'http://github.com/lyndatcd/Hell-world.git',
-//   optionsSuccessStatus: 200 
-// }
+const corsOptions = {
+	origin: 'http://github.com/lyndatcd/Hell-world.git',
+	optionsSuccessStatus: 200 
+}
 
 // Test
-// app.get('/api/welcome', cors(corsOptions), auth, (req, res) => {
-//   res.status(200).send("Welcome to Jobinaire 🙌 ");
-// });
+app.get('/api/welcome', cors(corsOptions), auth, (req, res) => {
+	res.status(200).send("Welcome to Jobinaire 🙌 ");
+});
 
 // user signup & login
 const authRoute = require('./routes/authentication');
@@ -33,14 +33,6 @@ app.use("/api/password-reset", passwordReset);
 
 const users = require("./routes/users");
 app.use("/api/users", users);
-
-// job search
-const searchRoute = require('./routes/job_search');
-app.use('/api/jobs', searchRoute);
-
-// job filter
-// const filterRoute = require('./routes/job_filter');
-// app.use('/api/jobs', filterRoute);
 
 module.exports = app;
 
